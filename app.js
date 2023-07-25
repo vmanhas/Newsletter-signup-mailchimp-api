4860d1efd0507575bed8b20d0fd2126e1f8eb41c
vmanhas <vishwajeetmanhas5091@gmail.com>
2023-07-25 13:18:30 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,62 @@
+
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
+const client = require("@mailchimp/mailchimp_marketing"); // you need to add dependency first. See tips.
+ 
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+ 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/sign-up.html");
+});
+ 
+client.setConfig({
+  apiKey: process.env.API_KEY,
+  server: process.env.SERVER_ID,
+});
+ 
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email
+  }
+ 
+  const run = async () => {
+    try {
+      const response = await client.lists.addListMember(process.env.AUDIENCE_ID, {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName
+        }
+      });
+      console.log(response);
+      res.sendFile(__dirname + "/success.html");
+    } catch (err) {
+      console.log(err.status);
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+ 
+  run();
+});
+ 
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+ 
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.");
+});
+
